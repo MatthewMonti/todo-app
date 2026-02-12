72d09e4cd93afe0f796634a5d1b01f0389f397de
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2026-02-12 16:45:22 -0500

    see an ordered printed numbered list

--- todo.py
+++ todo.py
@@ -1,5 +1,7 @@
 exit = False
 
+TO_DO_LIST = []
+
 def get_argument(command):
     command_name, argument = command.split(" ", maxsplit=1)
     return argument 
@@ -11,10 +13,22 @@ while not exit:
     elif command.startswith("add"):
         # add task
         argument = get_argument(command)
+        TO_DO_LIST.append(argument)
         print(f"Adding task: {argument}")
     elif command.startswith("delete"):
         # delete task
         argument = get_argument(command)
+        # specific pop over del (accident whole list) or remove (less specific)
+        ## integer convert string to number - 1 (index value minus 1)
+        TO_DO_LIST.pop(int(argument) -1)
+        ## int converts string to number
         print(f"Deleting task: {argument}")
+    elif command.startswith("list"):
+        index = 0
+        for todo_item in TO_DO_LIST:
+            index+=1
+            print(f"{index}. {todo_item}")
+        for index, item in enumerate(TO_DO_LIST):
+            print(f"{index}: {item}")
     else:
         print("Command not recognized")
