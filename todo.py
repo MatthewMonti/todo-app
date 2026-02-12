e6695e0797c55aa1d246964584e7e3d3ed4412a9
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2026-02-12 00:17:43 -0500

    argument for add task

--- todo.py
+++ todo.py
@@ -1,13 +1,18 @@
 exit = False
 
+def get_argument(command):
+    command_name, argument = command.split(" ", maxsplit=1)
+    return argument
+
 while not exit:
     command = input("Enter command: ")
 
     if command.startswith("exit"):
         exit = True
     elif command.startswith("add"):
-        pass
+        argument = get_argument(command)
+        print(f"Adding task: {argument}")
     elif command.startswith("delete"):
-        pass
+        argument = get_argument(command)
     else:
         print("Command not recognized")
