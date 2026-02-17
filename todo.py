b9822bc73bd4e803ad3dc222a106def8e570cc01
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2026-02-17 15:27:27 -0500

    simplify parameters

--- todo.py
+++ todo.py
@@ -7,17 +7,17 @@ def get_argument(command):
     return argument 
 
 
-def handle_add_command(TO_DO_LIST, get_argument, command):
+def handle_add_command(command):
     argument = get_argument(command)
     TO_DO_LIST.append(argument)
     print(f"Adding task: {argument}")
 
-def handle_delete_command(TO_DO_LIST, get_argument, command):
+def handle_delete_command(command):
     argument = get_argument(command)
     TO_DO_LIST.pop(int(argument) -1)
     print(f"Deleting task: {argument}")
 
-def handle_list_command(TO_DO_LIST):
+def handle_list_command():
     for index, item in enumerate(TO_DO_LIST):
         print(f"{index+1}: {item}")
 
@@ -27,14 +27,14 @@ while not exit:
     if command.startswith("exit"):
         exit = True
     elif command.startswith("add"):
-        handle_add_command(TO_DO_LIST, get_argument, command)
+        handle_add_command(command)
     elif command.startswith("delete"):
-        handle_delete_command(TO_DO_LIST, get_argument, command)
+        handle_delete_command(command)
         # NOTE: 
         # User input is always a string, so the delete command receives the index as a # numeric string (e.g., "3"). We convert that numeric string to an integer using # int(argument) so we can delete the correct item by its index. 
         # This matters because list values can repeat (e.g., two "walk" tasks), but 
         # indexes are unique. Deleting by index ensures we remove the exact item the # user intended, not just the first or last matching value.
     elif command.startswith("list"):
-        handle_list_command(TO_DO_LIST)
+        handle_list_command()
     else:
         print("Command not recognized")
