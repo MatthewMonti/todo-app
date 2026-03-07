5ee4d2cbfeb8ba82f18e135d90b5ef0dac115390
MatthewMonti <93679020+MatthewMonti@users.noreply.github.com>
2026-03-07 10:23:08 -0500

    argument cmd error handling updated

--- todo.py
+++ todo.py
@@ -1,5 +1,4 @@
 import sys
-
 exit = False
 filename = None
 TO_DO_LIST = []
@@ -12,14 +11,15 @@ def handle_add_command(command):
     try:
         command_name, argument = command.split(" ", maxsplit =1)
         part = command_name, argument
+
         if part[0].strip() != "add" and part[1].strip():
-            print('Check spelling of add command')
+            print("add command must be seperated from todo item name")
             return 
         if  part[0]== "add" and part[1].strip() == "":
             print('Assign todo item before adding todo item')
             return
         if part[0].strip() != "add" and part[1].strip() == "":
-            print('Check spelling of add command')
+            print("'add' command is not spelled right 02")
             return 
 
         if part[0] == "add" and part[1]:
@@ -30,52 +30,43 @@ def handle_add_command(command):
         return argument 
     
     except ValueError:
-        print("Seperate command from todo item")
-        return
+        valid_command = ['save', 'open', 'delete', 'add', 'list']
+        parts = command.split()
+
+        valid_space = [' ']
+
+        # print(parts[0])
+        if parts and parts[0] in valid_command:
+            print("Forgot to assign todo item")
+            return
+        elif parts and parts[0] not in valid_space:
+            print("only 'add' command is valid for inserting new todo itemsß")
+            return
     
 def handle_delete_command(command):
-    try: 
-        command_name, argument = command.split(" ", maxsplit =1)
-        part = command_name, argument
-        if part[0]!="delete" and part[1].strip():
-            print("Check spelling of delete command")
-            return 
-        if part[0].strip() == "delete" and part[1].strip() == "":
-            print("Assign todo item number integer before deleting todo item")
-            return 
-        if part[0].strip() != "delete" and part[1].strip() == "":
-            print('Check spelling of add command')
-            return 
-        
-        if part[0].strip() == "delete" and type(part[1].strip()) != int:
-            print('For proper deletion use todo item number not name')
-            return 
-        
-        if part[0] == "delete" and int(part[1]):
-            argument = get_argument(command)
-            TO_DO_LIST.pop(int(argument) -1)
-            print(f"Updated List:")
-            handle_list_command()
-            return
-        return argument
+    try:
+        item_number = get_argument(command)
+        num = int(item_number)
+        print(f"Deleting task: {num}")
+        TO_DO_LIST.pop(num - 1)
     except ValueError:
-        print("Seperate command from todo item")
-        return
+        print("forgot to provide a integer of todo item to delete")
 
 def handle_list_command():
     for index, item in enumerate(TO_DO_LIST):
         print(f"{index+1}: {item}")
 
-class handle_typo(Exception):
-    handle_list_command()
-
 def handle_save_command(command):
-    global filename
-    filename = get_argument(command)
-    with open(get_filename(filename), 'w') as f:
-        for i in TO_DO_LIST:
-            f.write(f'{i}\n')
-
+    try:
+        global filename
+        filename = get_argument(command)
+        with open(get_filename(filename), 'w') as f:
+            for i in TO_DO_LIST:
+                f.write(f'{i}\n')
+        raise ValueError
+    except ValueError:
+        print( "forgot to provide a filename to save")
+    
 def get_filename(filename):
     return filename + ".todo.txt"
 
@@ -99,13 +90,16 @@ def process_command(command):
 
     if command.startswith("exit"):
         exit = True
-    elif command.startswith("add"):
+    elif command.startswith("add" ):
         handle_add_command(command)
-    elif command.startswith("delete"):
+    elif command.startswith("delete "):
         handle_delete_command(command)
     elif command.startswith("list"):
-        handle_list_command()
-    elif command.startswith("open"):
+        if len(command) == 4:
+            handle_list_command()
+        else:
+            print(command , "Print list with comamand: list")
+    elif command.startswith("open "):
         handle_open_command(command)
         if len(TO_DO_LIST) > 1:
             while True: 
@@ -128,6 +122,7 @@ def process_command(command):
         handle_save_command(command)
     else:
         print("Command not recognized")
+
     if exit and filename:
         handle_save_command(f"save {filename}")
         print(f"Saved tasks to file {filename} before exiting.")
